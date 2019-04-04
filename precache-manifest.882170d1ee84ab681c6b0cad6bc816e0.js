5b3f05c3ecc77eb2dbf4182ba4ea9c1b4ebc5ca2
Mateusz Michalak <michalakweb@gmail.com>
2019-04-04 12:54:16 +0200

    Updates

---
+++ precache-manifest.882170d1ee84ab681c6b0cad6bc816e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bbbaa44e5d1582bc26b",
+    "url": "/WordpressReact/static/css/main.39b7ac36.chunk.css"
+  },
+  {
+    "revision": "5bbbaa44e5d1582bc26b",
+    "url": "/WordpressReact/static/js/main.730ed05c.chunk.js"
+  },
+  {
+    "revision": "cbbc73332ce21e089ceb",
+    "url": "/WordpressReact/static/js/runtime~main.ef13217f.js"
+  },
+  {
+    "revision": "f896d6c440cae8afedc2",
+    "url": "/WordpressReact/static/css/2.a16b4b39.chunk.css"
+  },
+  {
+    "revision": "f896d6c440cae8afedc2",
+    "url": "/WordpressReact/static/js/2.4da5acfd.chunk.js"
+  },
+  {
+    "revision": "f369b0052be56138d3ede4d21e674a07",
+    "url": "/WordpressReact/static/media/logo.f369b005.svg"
+  },
+  {
+    "revision": "8fd4ca4b0170f52a61b5ce5465e0566a",
+    "url": "/WordpressReact/index.html"
+  }
+];
